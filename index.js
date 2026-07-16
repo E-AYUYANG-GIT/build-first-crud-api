f2a03acef9bcbb93b15ca958f3bb7a83b76c195b
ELI-YU-0515 <elizaryu15@gmail.com>
2026-07-16 15:30:33 +0800

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get ('/', (req, res) => {
+    res.json(
+        { 
+            name: 'Task API',
+            version: '1.0.0',
+            endpoints: ["/tasks"]
+        });
+});
+
 app.get('/health', (req, res) => {
-    res.json('Hello World!');
+    res.json(
+        {
+            status: 'OK',
+        }
+    );
 })
 
 app.listen(port, () => {

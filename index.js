b19324fa32c91d6d3466ea564e2ac0a0d0e03cc0
ELI-YU-0515 <elizaryu15@gmail.com>
2026-07-16 16:15:31 +0800

    read endpoints with 404

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let tasks = [
+    {"id": 1, "title": "Product Inventory", "done": true},
+    {"id": 2, "title": "Patient Records", "done": true},
+    {"id": 3, "title": "Prescription Management", "done": true}
+];
 app.get ('/', (req, res) => {
     res.json(
         { 
@@ -9,6 +14,21 @@ app.get ('/', (req, res) => {
             version: '1.0.0',
             endpoints: ["/tasks"]
         });
+});
+
+app.get ('/tasks', (req, res) => {
+    res.json(tasks);
+});
+app.get ('/tasks/:id', (req, res) => {
+    const requestedId = parseInt(req.params.id); 
+
+    const task = tasks.find(t => t.id === requestedId);
+
+    if (task) {
+        res.json(task);
+    } else {
+        res.status(404).json({ message: "Task not found" });
+    }
 });
 
 app.get('/health', (req, res) => {

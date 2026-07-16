709d8ccd788db2f206d87ac35457b147160bcda6
ELI-YU-0515 <elizaryu15@gmail.com>
2026-07-17 00:43:23 +0800

    Stage 4 full CRUD

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ app.get ('/', (req, res) => {
 });
 
 app.get ('/tasks', (req, res) => {
-    res.json(tasks);
+    res.status(200).json(tasks); //Updated to inlcude status code 200
 });
+
 app.get ('/tasks/:id', (req, res) => {
     const requestedId = parseInt(req.params.id); 
 
@@ -31,6 +32,14 @@ app.get ('/tasks/:id', (req, res) => {
         res.status(404).json({ message: "Task not found" });
     }
 });
+
+//STAGE 3
+//Use Powershell Native Command to test
+// Invoke-RestMethod `
+//     -Uri "http://localhost:3000/tasks" `
+//     -Method POST `
+//     -ContentType "application/json" `
+//     -Body '{"title":"Buy milk"}'
 
 app.post('/tasks', (req, res) => {
     const { title } = req.body;
@@ -51,6 +60,46 @@ app.post('/tasks', (req, res) => {
 
     res.status(201).json(newTask);
 });
+
+app.put('/tasks/:id', (req, res) => {
+    const findId = parseInt(req.params.id);
+    const findTask = tasks.find(t => t.id === findId);
+
+    if (!findTask) {
+        return res.status(404).json({error: "Unknown Id"});
+    } 
+
+    const { title, done } = req.body;
+    
+    if (
+        (title === undefined && done === undefined) ||
+        (title === undefined || title.trim() === "")
+    ) {
+        return res.status(400).json({error: "Empty/Invalid Id"});
+    }
+    if (title !== undefined){
+        findTask.title = title;
+    }
+    if (done !== undefined){
+        findTask.done = done;
+    }
+
+    res.status(200).json(findTask);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    const findId = parseInt(req.params.id);
+    const taskIndex = tasks.findIndex(t => t.id === findId);
+
+    if (taskIndex === -1) {
+        return res.status(404).json({error: "Unknown Id"})
+    }
+    
+    tasks.splice(taskIndex, 1);
+
+    res.status(204).json({error: "No Content - Success"})
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
